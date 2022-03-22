9f8c4cf8dd1549aeb0334ed7c0349e03a07d4786
DecadentCode <newswithdrew@gmail.com>
2022-03-22 13:22:45 -0400

    Finished Dragon Cave, need to fix Exit Code 1

--- Dragon Cave/src/main/java/Main.java
+++ Dragon Cave/src/main/java/Main.java
@@ -2,12 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        declarations
         Scanner input = new Scanner(System.in);
+
+//        output introduction
         System.out.println("You are in a land full of dragons. In front of you,\n" +
                 "you see two caves. In one cave, the dragon is friendly\n" +
                 "and will share his treasure with you. The other dragon\n" +
                 "is greedy and hungry and will eat you on sight.\n" +
                 "Which cave will you go into? (1 or 2)");
+
+//        input player choice
         int choice = input.nextInt();
+
+//        output results of choice
+        System.out.println("You approach the cave...\n" +
+                "It is dark and spooky...\n" +
+                "A large dragon jumps out in front of you! He opens his jaws and...");
+        if (choice == 1){
+            System.out.println("Gobbles you down in one bite!");
+        }else if(choice == 2){
+            System.out.println("Welcomes you to his home and some of his treasure!");
+        }else{
+            System.out.println("Gives you a stern talking to for not following\n" +
+                    "the directions correctly, then he eats you!");
+        }
     }
 }
