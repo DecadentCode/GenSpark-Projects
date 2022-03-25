df6f043f8e6cc67354a99775aaf41440c54ab962
DecadentCode <newswithdrew@gmail.com>
2022-03-25 11:48:06 -0400

    Added catch blocks to Number Guesser

--- Number Guesser/src/main/java/Main.java
+++ Number Guesser/src/main/java/Main.java
@@ -4,45 +4,70 @@ public class Main {
     public static void main(String[] args){
 //        declarations
         Scanner input = new Scanner(System.in);
+        String name;
         int rng;
         int guess;
-        int counter;
-        boolean correct=false;
-        String replay="y";
+        int guesses;
+        String replay;
 
 //        retrieve player name
         System.out.println("Hello! What is your name?");
-        String name = input.nextLine();
+        try {
+            name = input.nextLine();
+        }catch (Exception nameError){
+            System.out.println("Input error: "+nameError+"\n" +
+                    "Something went wrong, so your name shall be Alex");
+            name = "Alex";
+        }
 
 //        the guessing game
-        while(replay.contains("y")){
-            rng = (int)Math.floor(Math.random()*(20-1)+1);
-            guess = 0;
-            counter = 0;
+        do{
+            guesses = 0; //resets the number of attempts
+            guess = 0; //resets the player's guess
+            rng = (int)Math.floor(Math.random()*(20-19+1)+19); //generates a new random number
             System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
-            for (int i = 0; i < 6 && !correct; i++) {
-                counter++;
+            for (int i = 0; i < 6 && guess != rng; i++) {
+                guesses++;
                 System.out.println("Take a guess.");
-                guess = input.nextInt();
-                if (guess > rng) {
-                    System.out.println("Your guess is too high.");
-                } else if (guess < rng) {
-                    System.out.println("Your guess is too low.");
-                } else {
-                    correct = true;
+                try {
+                    guess = input.nextInt();
+                    if (guess > rng) {
+                        System.out.println("Your guess is too high.");
+                    } else if(guess < rng){
+                        System.out.println("Your guess is too low.");
+                    }
+                }catch (Exception guessError){
+                    System.out.println("Input error: "+guessError+"\n" +
+                            "Something went wrong with your guess.");
+                    guess=0;
                 }
+                input.nextLine(); // resets the input from the previous nextInt()
             }
 
-//        output game results
-            if (correct) {
-                System.out.println("Good job, " + name + "! You guessed my number in " + counter + " guesses!");
+//            output results
+//            changes message based on number of attempts
+            if(guesses==1){
+                System.out.println("Good job, " + name + "! You guessed my number on the first try!");
+            }else if(guess==rng){
+                System.out.println("Good job, " + name + "! You guessed my number in " + guesses + " guesses!");
             }else{
                 System.out.println("So sorry, " + name + ", but you failed 6 times...");
             }
-            correct = false;
+
+//            would the player like to play again?
             System.out.println("Would you like to play again? (y or n)");
-            input.nextLine(); // resets the input from the previous nextInt()
-            replay = input.nextLine();
-        };
+            try {
+                replay = input.nextLine();
+            }catch (Exception replayError){
+                System.out.println("Input Error: "+replayError+"\n" +
+                        "Something went wrong, ending game.\n" +
+                        "End of Line.");
+                replay="n";
+            }
+        }while(replay.contains("y"));
+
+//        ending statement
+        System.out.println("Thanks for playing, "+name+"!\n" +
+                "End of line.");
     }
 }
